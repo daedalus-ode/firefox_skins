b9226635ab5db11f8f949011fbd1941f437f7020
daedeleus <ptv1324ptv@outlook.com>
2024-07-18 00:13:34 +0530

    Added user.js for config setup

---
+++ user.js
@@ -0,0 +1,30 @@
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// popup search
+user_pref("uc.tweak.popup-search", true);
+
+// Extend sidebar length to 250px
+user_pref("uc.tweak.longer-sidebar", true);
+
+// Hide header sidebar, containing space/tab panel name
+user_pref("uc.tweak.hide-sidebar-header", true);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// CSS's `:has()` selector 
+user_pref("layout.css.has-selector.enabled", true);
+
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
+
+// Integrated unit convertor at urlbar
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+// Trim  URL
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimURLs", true);
+
+// GTK rounded corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
