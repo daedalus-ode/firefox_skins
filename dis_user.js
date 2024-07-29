35fcde1f219a1e965cad405b85bc506054f91a91
daedeleus <ptv1324ptv@outlook.com>
2024-07-29 23:49:24 +0530

    Added user.js

---
+++ dis_user.js
@@ -0,0 +1,5 @@
+
+user_pref("animatedFox.centeredTabs", false);
+user_pref("animatedFox.centeredUrl", false);
+user_pref("animatedFox.roundedCorners", false);
+user_pref("animatedFox.showTabCloseButton", false);
---
+++ user.js
@@ -0,0 +1,6 @@
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("animatedFox.centeredTabs", true);
+user_pref("animatedFox.centeredUrl", true);
+user_pref("animatedFox.roundedCorners", true);
+user_pref("animatedFox.showTabCloseButton", true);
