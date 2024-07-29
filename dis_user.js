51233cb106022ccb63d5ca3b5d2cdb402f385c23
daedeleus <ptv1324ptv@outlook.com>
2024-07-29 23:38:01 +0530

    Updated theme from github

---
+++ dis_user.js
@@ -0,0 +1,13 @@
+
+/* Default rules */
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", false);
+user_pref("svg.context-properties.content.enabled", false);
+user_pref("uc.tweak.popup-search", false);
+user_pref("uc.tweak.longer-sidebar", false);
+user_pref("uc.tweak.floating-tabs", false);
+user_pref("uc.tweak.newtab-background", false);
+user_pref("uc.tweak.disable-drag-space", false);
+user_pref("uc.tweak.hide-forward-button", false);
+user_pref("uc.tweak.hide-newtab-logo", false);
+user_pref("uc.tweak.show-tab-close-button-on-hover", false);
+user_pref("uc.tweak.show-tab-close-button-on-hover", false);
---
+++ user.js
@@ -0,0 +1,13 @@
+
+/* Default rules */
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("uc.tweak.popup-search", true);
+user_pref("uc.tweak.longer-sidebar", true);
+user_pref("uc.tweak.floating-tabs", true);
+user_pref("uc.tweak.newtab-background", true);
+user_pref("uc.tweak.disable-drag-space", true);
+user_pref("uc.tweak.hide-forward-button", true);
+user_pref("uc.tweak.hide-newtab-logo", true);
+user_pref("uc.tweak.show-tab-close-button-on-hover", true);
+user_pref("uc.tweak.show-tab-close-button-on-hover", true);
