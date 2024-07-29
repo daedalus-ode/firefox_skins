6e992a32a310f3597e3db462533ac7d40af2f6b1
daedeleus <ptv1324ptv@outlook.com>
2024-07-29 23:04:36 +0530

    Updated theme from github

---
+++ dis_user.js
@@ -0,0 +1,16 @@
+/* Source file made available under Mozilla Public License v. 2.0 See the main repository for updates as well as full license text. 
+   https://github.com/Godiesc/opera-gx */
+
+/* Default rules */
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", false); 
+user_pref("layout.css.color-mix.enabled", false);
+user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 3000);
+user_pref("layout.css.backdrop-filter.enabled", true);
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", true);
+user_pref("browser.newtabpage.activity-stream.newtabWallpapers.enabled", true);
+
+/*To active container tabs without any extension */
+user_pref("privacy.userContext.enabled", false);
+user_pref("privacy.userContext.ui.enabled", false);
+user_pref("privacy.userContext.longPressBehavior", 2);
